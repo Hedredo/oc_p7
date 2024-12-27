a4a20ecf2207a6497e8d06e3ceab680bd355e841
hedredo <lekeuj@hotmail.com>
2024-12-27 13:59:04 +0100

    update the local app

--- app_local/front.py
+++ app_local/front.py
@@ -5,8 +5,6 @@ import logging
 from dotenv import load_dotenv
 import os
 from enum import Enum
-
-# pip install opencensus opencensus-ext-logging opencensus-ext-azure
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 # Configure la classe Step pour le changement dynamique des boutons
@@ -20,20 +18,27 @@ class Step(Enum):
         next_value = (current_value.value % len(cls)) + 1
         return Step(next_value)
 
+# Création de la fonction logger avec le résultat mis en cache pour éviter d'ajouter plusieurs fois le même handler
+@st.cache_resource
+def get_logger():
+    logger = logging.getLogger(__name__)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        logger.addHandler(AzureLogHandler(connection_string=f"InstrumentationKey={instrumentation_key}"))
+    return logger
+
 # Récupère les variables de session
 load_dotenv()
 api_url = os.getenv("API_URL")
 instrumentation_key = os.getenv("INSTRUMENTATION_KEY")
 
-# Configure le journal pour envoyer des traces à Application Insights
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(AzureLogHandler(connection_string=f"InstrumentationKey={instrumentation_key}"))
+# Récupère ou initialise le logger
+logger = get_logger()
 
 # Streamlit : Configure l'interface utilisateur affiché par défaut
 st.title("Live Tweet Sentiment Analysis")
 
-
+# Initialise les session state lors du premier run de script
 if "step" not in st.session_state:
     st.session_state.step = Step.INITIAL
 if "prediction_data" not in st.session_state:
@@ -41,13 +46,32 @@ if "prediction_data" not in st.session_state:
 if "text" not in st.session_state:
     st.session_state.text = ""
 
-def handle_on_click():
+# Fonction handle_on_click : gère les actions de boutons en fonction des états de session
+def handle_on_click(message=None):
+    match message:
+        case "Yes":
+            logger.info("Good prediction", extra={
+                "custom_dimensions": {
+                    "text": st.session_state.prediction_data["text"],
+                    "prediction": st.session_state.prediction_data["prediction"],
+                }
+            })
+        case "No":
+            logger.warning("Bad prediction", extra={
+                "custom_dimensions": {
+                    "text": st.session_state.prediction_data["text"],
+                    "prediction": st.session_state.prediction_data["prediction"],
+                }
+            })
+        case _:
+            pass
+        
     if st.session_state.step == Step.FEEDBACK:
         st.session_state.step = Step.INITIAL
     else:
         st.session_state.step = Step.next_value(st.session_state.step)
 
-# Ajout d'un bouton pour effectuer l'action de prédiction du sentiment
+# session_state INITIAL
 if st.session_state.step.value == Step.INITIAL.value:
     st.session_state.text = ""
     text = st.text_input("Enter a tweet please:")
@@ -57,6 +81,7 @@ if st.session_state.step.value == Step.INITIAL.value:
         on_click=handle_on_click,
     )
 
+# session_state PREDICT
 elif st.session_state.step.value == Step.PREDICT.value:
     url = f"{api_url}/predict"
     data = {"text": st.session_state.text}
@@ -64,10 +89,10 @@ elif st.session_state.step.value == Step.PREDICT.value:
 
     if response.status_code != 200:
         st.error(
-            "Error occurring the connection. We will fix the problem ASAP. Please try again later."
+            "Unable to establish the connection. We will fix the problem ASAP. Please reload the page and try again later."
         )
         logger.error(
-            "Error occurring the API call.", extra={
+            "Error : API call", extra={
                 "custom_dimensions": {
                     "status_code": response.status_code,
                     "request": "predict",
@@ -76,11 +101,12 @@ elif st.session_state.step.value == Step.PREDICT.value:
     else:
         prediction = response.json()["sentiment"]
     
-    # Display the result
+    # Affiche du texte
     st.write(f"Tweet entered: {st.session_state.text}")
     st.write("Model computing the sentiment of the tweet...")
-    time.sleep(2)
-    # Display the result
+    time.sleep(2) # Simulation d'un temps de calcul
+
+    # Affiche le résultat de la prédiction
     match prediction:
         case "positive":
             st.success(f"Sentiment predicted: {prediction}")
@@ -95,30 +121,21 @@ elif st.session_state.step.value == Step.PREDICT.value:
         "prediction": prediction,
     }
 
+    # Affiche la question de feedback
     st.write("Was the prediction correct?")
-    # Affiche les boutons côté à côte
+    # Affiche les boutons de feedback côté à côte
     leftcol, midcol, rightcol = st.columns(3)
+    # Affiche le bouton de gauche
     with leftcol:
-        st.button("Yes", on_click=handle_on_click)
-        logger.info("Good prediction", extra={
-            "custom_dimensions": {
-                "text": st.session_state.prediction_data["text"],
-                "prediction": st.session_state.prediction_data["prediction"],
-            }
-        })
-    
+        st.button("Yes", on_click=handle_on_click, args=("Yes",), icon="\U0001F44D", use_container_width=True)
+    # Affiche le bouton du milieu
     with midcol:
-        st.button("No", on_click=handle_on_click)
-        logger.warning("Bad prediction", extra={
-            "custom_dimensions": {
-                "text": st.session_state.prediction_data["text"],
-                "prediction": st.session_state.prediction_data["prediction"],
-            }
-        })
-
+        st.button("No", on_click=handle_on_click, args=("No",), icon="\U0001F44E", use_container_width=True)
+    # Affiche le bouton de droite
     with rightcol:
-        st.button("I don't know", on_click=handle_on_click)
+        st.button("I don't know", on_click=handle_on_click, use_container_width=True)
 
+# session_state FEEDBACK
 else:
     st.write("Thank you for your feedback!")
     st.button("Try another tweet !", on_click=handle_on_click)
