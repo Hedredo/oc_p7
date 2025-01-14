e3d25bd4086181fb909d624590d038138b0da242
hedredo <lekeuj@hotmail.com>
2025-01-14 17:44:17 +0100

    update the test with latest models

--- tests/tf_app/test_predict.py
+++ tests/tf_app/test_predict.py
@@ -1,27 +1,28 @@
 import unittest
 import tensorflow as tf
-from tf_app.model import custom_standardization, set_model
+from tf_app.model import custom_standardization_punct, set_model, SpacyTokenizer
 
 class TestPredict(unittest.TestCase):
 
     def setUp(self):
-        self.model = set_model()
+        self.model = set_model("./models/neuralnet")
+        self.tokenizer = SpacyTokenizer("./models/spacy_en_core_web_sm")
 
-    def test_custom_standardization(self):
-        input_text = tf.constant(["Hello @user! Check out http://example.com"])
+    def test_custom_standardization_punct(self):
+        input_text = tf.constant(["Hello check out http://example.com"])
         expected_output = tf.constant(["hello check out"])
-        output = custom_standardization(input_text)
+        output = custom_standardization_punct(input_text)
         self.assertTrue(tf.reduce_all(tf.equal(output, expected_output)))
 
     def test_model_prediction_positive(self):
-        input_text = tf.constant(["I love this product!"])
-        probability = self.model.predict(input_text)
+        text = "I love this product!"
+        probability = self.model.predict(self.tokenizer.tokenize([text]))
         sentiment = "positive" if probability[0][0] > 0.5 else "negative"
         self.assertEqual(sentiment, "positive")
 
     def test_model_prediction_negative(self):
-        input_text = tf.constant(["I hate this product!"])
-        probability = self.model.predict(input_text)
+        text = "I hate this product!"
+        probability = self.model.predict(self.tokenizer.tokenize([text]))
         sentiment = "positive" if probability[0][0] > 0.5 else "negative"
         self.assertEqual(sentiment, "negative")
         
