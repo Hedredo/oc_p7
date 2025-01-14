36f2735006eb8c83c14da57b859cc63b974efa5e
hedredo <lekeuj@hotmail.com>
2025-01-14 17:25:15 +0100

    update tf_model_application

--- tf_app/main.py
+++ tf_app/main.py
@@ -1,11 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
-import tensorflow as tf
-from model import set_model, custom_standardization
+from model import set_model, SpacyTokenizer, custom_standardization_punct
 
-# Load the model
-model = set_model() # Assign the function model_path argument with the another model if needed
+# Load the model & tokenizer
+model = set_model("./models/neuralnet") # Assign the function model_path argument with the another model if needed
+tokenizer = SpacyTokenizer("./models/spacy_en_core_web_sm")
 
 # Create the FastAPI instance
 app = FastAPI()
@@ -23,7 +23,7 @@ def read_root() -> Union[dict, str]:
 def predict(data: TextData) -> dict:
     text = data.text
     # Make the prediction
-    probability = model.predict(tf.constant([text]))
+    probability = model.predict(tokenizer.tokenize([text]))
     sentiment = "positive" if probability[0][0] > 0.5 \
         else ("negative" if probability[0][0] < 0.5 else "neutral")
     return {"text": text, "sentiment": sentiment} # "probability": float(prediction[0][0])
--- tf_app/model.py
+++ tf_app/model.py
@@ -1,16 +1,44 @@
 import mlflow.tensorflow
 import tensorflow as tf
+import mlflow.spacy
+from typing import List
 
-@tf.keras.utils.register_keras_serializable(package="custom_text_func", name="custom_standardization")
-def custom_standardization(tensor):
+class SpacyTokenizer:
+    def __init__(self, model_uri: str):
+        self.nlp = mlflow.spacy.load_model(model_uri)
+
+    def tokenize(self, texts: List[str], lemmatize=False):
+        # Error handling for input
+        assert isinstance(texts, list), "Input must be a list of strings"
+        assert isinstance(lemmatize, bool), "lemmatize must be a boolean"
+        assert isinstance(texts[0], str), "List elements must be strings"
+        # Tokenize the texts
+        tokenized_texts = []
+        # Tokenize and lemmatize
+        if lemmatize:
+            for doc in self.nlp.pipe(texts, disable=["ner"]):
+                tokenized_texts.append(" ".join([token.lemma_ for token in doc]))
+        # Tokenize the text only
+        else:
+            for doc in self.nlp.pipe(texts, disable=["ner", "lemmatizer"]):
+                tokenized_texts.append(" ".join([token.text for token in doc]))
+        # Return the tokenized texts
+        return tokenized_texts
+
+@tf.keras.utils.register_keras_serializable(package="custom_text_func", name="custom_standardization_punct")
+def custom_standardization_punct(tensor):
     tensor = tf.strings.lower(tensor)  # lowercase
-    tensor = tf.strings.regex_replace(tensor, r"@\w+", " ")  # strip mentions
-    tensor = tf.strings.regex_replace(tensor, r"http\S+|www\S+", " ")  # strip urls
-    tensor = tf.strings.regex_replace(tensor, r"[^\w\s\d]", " ")  # strip punctuation
+    tensor = tf.strings.regex_replace(tensor, r"https?://?\S+", " ")  # strip mentions
+    tensor = tf.strings.regex_replace(tensor, r"www\.\w+\S+", " ")  # strip mentions
+    tensor = tf.strings.regex_replace(tensor, r"@\w+", "@usertag")
+    tensor = tf.strings.regex_replace(tensor, r"#\w+", " ")
+    tensor = tf.strings.regex_replace(tensor, r"(\w+)[.,!?;](\w+)", r"\1 \2")
     tensor = tf.strings.regex_replace(tensor, r"\s{2,}", " ")  # strip multiple spaces
     return tf.strings.strip(tensor)  # strip leading and trailing spaces
 
+
+
 # Load the model
-def set_model(model_path="./mlflow/689416981458083287/b5efda2cee954ff1a88923f357bc0525/artifacts/model"):
+def set_model(model_path: str):
     model = mlflow.tensorflow.load_model(model_path) # Load the model with the MLflow Keras API
     return model
